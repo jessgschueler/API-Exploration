48e00e2e888ef08d65d7bbdfc271650e4cd98f0d
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-05-27 14:46:18 -0700

    Add more comments, write to csv.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import pandas as pd
 
+#establish dataframe
 super_data = [
        {"name": "Spiderman", "superpower": "Bitten by spider", "weakness": "big dumb"},
        {"name": "Thor", "superpower": "being a god", "weakness": "unclear"},
@@ -9,6 +10,7 @@ super_data = [
 super_df  = pd.DataFrame(super_data)
 super_df.set_index(keys="name", drop=False, inplace=True)
 
+#create flask instance
 app = Flask(__name__)
 app.config["db"]=super_df
 
@@ -19,17 +21,21 @@ def show_stats():
     """
     #access pandas data frame
     global super_df
+    #get parameters
     name = request.args.get("name", default=None)
     superpower = request.args.get("superpower", default=None)
     weakness = request.args.get("weakness", default=None)
+    #if statement that allows for search by name, superpower or weakness
     if name is not None:
         result_df = super_df.loc[super_df["name"]==name]
     elif superpower is not None:
         result_df = super_df.loc[super_df["superpower"]==superpower]
     elif weakness is not None:
         result_df = super_df.loc[super_df["weakness"]==weakness]
+    #if no parameters are provided, return entire dataframe
     else:
         result_df = super_df
+    #create response json and headers
     resp_json = {
         "result": result_df.to_dict(orient="records")
     }
@@ -44,18 +50,25 @@ def create_hero():
     """
     HTTP POST: Function to add new superhero data
     """
+    #access data frame
     global super_df
     try:
+        #empty lists for data to be stores in
         inserted_hero = []
         rejected_hero = []
+        #defining out data as the input json
         data = request.json
+        #looping through input, setting index for new hero and adding new row
         for hero in data:
             if ("name" in hero) and ("superpower" in hero) and ("weakness" in hero):
                 index = hero["name"]
                 super_df.loc[hero["name"]] = hero
+                #append to accepted list
                 inserted_hero.append(hero)
+            #if needed data is not present append to rejects list
             else:
                 rejected_hero.append(hero)
+        #create response json and headers
         resp_json = {
             "records_inserted": len(inserted_hero),
             "result": inserted_hero,
@@ -65,9 +78,10 @@ def create_hero():
             "content-type": "application/json",
         }
         return resp_json, 200, resp_headers
+    #return error if above cannot be performed.
     except Exception as err:
         return {"status": "error", "error_msg": str(err)}, 400, {"content-type": "application/json"}
 
-
+#run function
 if __name__ == "__main__":
     app.run('0.0.0.0', 5050)
